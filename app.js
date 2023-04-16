9d8109c6242cc02991b8343aa810c3b2d659165a
Elizabeth Heideman <77503008+ElizaHeidi@users.noreply.github.com>
2023-04-16 11:53:41 -0500

    Add trash button

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let input = document.querySelector("#book-input");
 let submit = document.querySelector(".submit-btn");
 let bookArray = [];
+let table;
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -8,8 +9,6 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
-
-// create new Book based on user input
 
 submit.addEventListener("click", function (event) {
   event.preventDefault();
@@ -22,54 +21,58 @@ submit.addEventListener("click", function (event) {
   let pages = bookInputArray[2];
   let read = bookInputArray[3];
 
-  let newBook = new Book(title, author, pages, read); // pass the arguments to the constructor
-  bookArray.push(newBook); // push the new Book object to the array instead of the input array
+  let newBook = new Book(title, author, pages, read);
+  bookArray.push(newBook);
 
   console.log(bookArray);
-  if (bookArray != []) {
-    addRow();
-  } else {
+
+  if (!table) {
     createTable();
+  } else {
+    addRow(newBook);
   }
+
+  const trashBtn = document.createElement("button");
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  trashBtn.classList.add("trash-icon");
+
+  const icon = document.createElement("i");
+  icon.classList.add("fa-solid", "fa-trash");
+  trashBtn.appendChild(icon);
+
+  row.appendChild(trashBtn);
+
+  clear();
 });
 
+// Functions
+
 function createTable() {
-  let table = document.createElement("table");
+  table = document.createElement("table");
 
-  // add headers to the table
   let headers = ["Title", "Author", "Pages", "Read"];
-  let header = table.createTHead();
-  let headerRow = header.insertRow(0);
-  for (let i = 0; i < headers.length; i++) {
-    headerRow.insertCell(i).innerHTML = headers[i];
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of headers) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
   }
 
-  // add books to the table
-  for (let i = 0; i < bookArray.length; i++) {
-    let row = table.insertRow(i + 1); // add 1 to i to avoid overwriting the header row
-    row.insertCell(0).innerHTML = bookArray[i].title;
-    row.insertCell(1).innerHTML = bookArray[i].author;
-    row.insertCell(2).innerHTML = bookArray[i].pages;
-    row.insertCell(3).innerHTML = bookArray[i].read;
-  }
-
+  addRow(bookArray[0]);
   document.body.append(table);
 }
 
-function addRow() {
-  let bookInput = input.value;
-  console.log(bookInput);
-  let bookInputArray = bookInput.split(",");
-
-  let title = bookInputArray[0];
-  let author = bookInputArray[1];
-  let pages = bookInputArray[2];
-  let read = bookInputArray[3];
-
-  let table = document.createElement("table");
+function addRow(Book) {
   let row = table.insertRow();
-  row.insertCell(0).innerHTML = bookArray[0].title;
-  row.insertCell(1).innerHTML = bookArray[1].author;
-  row.insertCell(2).innerHTML = bookArray[2].pages;
-  row.insertCell(3).innerHTML = bookArray[3].read;
+  row.insertCell(0).innerHTML = Book.title;
+  row.insertCell(1).innerHTML = Book.author;
+  row.insertCell(2).innerHTML = Book.pages;
+  row.insertCell(3).innerHTML = Book.read;
+}
+
+function clear() {
+  input.value = "";
 }
